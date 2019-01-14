eb8b8cc53d13ce8dd25a615719840ab67ac9cd6d
Eros <george.marr.cobc@gmail.com>
2019-01-14 23:56:17 +0000

    feck it ya know

---
+++ usb_list.c
@@ -0,0 +1,5 @@
+//
+// Created by George on 14-Jan-19.
+//
+
+#include "usb_list.h"
---
+++ usb_list.h
@@ -0,0 +1,12 @@
+//
+// Created by George on 14-Jan-19.
+//
+
+#ifdef __cplusplus
+    extern "C" {};
+#endif
+
+#include <tchar.h>
+#include <setupapi.h>
+#include <initguid.h>
+#include <stdio.h>
