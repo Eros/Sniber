5cf97379f8e1230841a02be19e9241a6fd8c33d3
Eros <george.marr.cobc@gmail.com>
2019-01-15 00:16:27 +0000

    handle reading from the bulk point

--- endpoint.cpp
+++ endpoint.cpp
@@ -4,8 +4,28 @@
 
 #include <windows.h>
 
-BOOL readFromBulkEndpoint(WINUSB_INTERFACE, hDeviceHandle, UCHAR* pID, ULONG cbSize) {
+BOOL readFromBulkEndpoint() {
+
+    WINUSB_INTERFACE hDeviceHandle;
+    UCHAR* pID;
+    ULONG cbSize;
+
     if(hDeviceHandle == INVALID_HANDLE_VALUE) {
         return false;
+    } else {
+        BOOL bResult = true;
+        UCHAR* szBuffer = (UCHAR*)LocalAlloc(LPTR, sizeof(UCHAR)*cbSize);
+        ULONG cbRead = 0;
+        bResult = WinUsb_ReadPipe(hDeviceHandle, *pID, szBuffer, cbSize, &cbRead, 0);
+
+        if(!bResult){
+            goto done;
+        }
+
+        printf("Read from pipe %d: %s \n Actual data: %d \n", *pID, szBuffer, cbRead);
+
+        done:
+            LocalFree(szBuffer);
+        return bResult;
     }
 }
